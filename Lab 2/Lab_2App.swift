b9ab6aec63a0b52f7b878bc31903a56a62babf01
Keaton-Harvey <123423711+Keaton-Harvey@users.noreply.github.com>
2024-10-06 18:13:26 -0500

    Initial Commit

---
+++ Lab 2/Lab_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_2App.swift
+//  Lab 2
+//
+//  Created by Keaton Harvey on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
